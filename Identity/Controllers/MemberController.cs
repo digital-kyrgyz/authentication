e033a44cc4f118ac614df04788e232b0f0fb7a91
Melis Archabaev <melis99mrx@gmail.com>
2022-05-04 00:01:57 +0600

    Password change profile was created

--- Identity/Controllers/MemberController.cs
+++ Identity/Controllers/MemberController.cs
@@ -1,14 +1,66 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Identity.Models;
+using Identity.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-
+using System.Threading.Tasks;
+using Mapster;
 namespace Identity.Controllers
 {
     [Authorize]
     public class MemberController : Controller
     {
+        private UserManager<AppUser> _userManager { get; }
+        private SignInManager<AppUser> _signInManager { get; }
+
+        public MemberController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
         public IActionResult Index()
         {
+            AppUser user = _userManager.FindByNameAsync(User.Identity.Name).Result;
+            UserVm userVm = user.Adapt<UserVm>();
+            return View(userVm);
+        }
+        [HttpGet]
+        public IActionResult PasswordChange()
+        {
             return View();
+        }
+        [HttpPost]
+        public IActionResult PasswordChange(PasswordChangeVm passwordVm)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser user = _userManager.FindByNameAsync(User.Identity.Name).Result;
+                
+                    bool exist = _userManager.CheckPasswordAsync(user, passwordVm.PasswordOld).Result;
+                    if (exist)
+                    {
+                        IdentityResult result = _userManager.ChangePasswordAsync(user, passwordVm.PasswordOld, passwordVm.PasswordNew).Result;
+                        if (result.Succeeded)
+                        {
+                            _userManager.UpdateSecurityStampAsync(user);
+                            _signInManager.SignOutAsync();
+                            _signInManager.PasswordSignInAsync(user, passwordVm.PasswordNew, true, false);
+
+                            ViewBag.Success = "true";
+                        }
+                        else
+                        {
+                            foreach (var item in result.Errors)
+                                ModelState.AddModelError("", item.Description);
+                        }
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "Эски сыр туура эмес");
+                    }
+            }
+            return View(passwordVm);
         }
     }
 }
---
+++ Identity/ViewModels/PasswordChangeVm.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Identity.ViewModels
+{
+    public class PasswordChangeVm
+    {
+        [Display(Name = "Эски сыр созунуз")]
+        [Required(ErrorMessage = "Эски сыр созду жазыныз")]
+        [DataType(DataType.Password)]
+        [MinLength(8, ErrorMessage ="Сыр соз эн аз 8 символ болуусу шарт")]
+        public string PasswordOld { get; set; }
+        [Display(Name = "Жаны сыр созунуз")]
+        [Required(ErrorMessage = "Жаны сыр созду жазыныз")]
+        [DataType(DataType.Password)]
+        [MinLength(8, ErrorMessage = "Сыр соз эн аз 8 символ болуусу шарт")]
+        public string PasswordNew { get; set; }
+
+        [Display(Name = "Салыштыруучу сыр созунузду жазыныз")]
+        [Required(ErrorMessage = "Жаны сыр созунду кайра жазыныз")]
+        [DataType(DataType.Password)]
+        [MinLength(8, ErrorMessage = "Сыр соз эн аз 8 символ болуусу шарт")]
+        [Compare("PasswordNew", ErrorMessage = "Жаны сыр созунуз, экоо бири-бирине дал келбей жатат")]
+        public string PasswordCompare { get; set; }
+    }
+}
