0eae45da7d27c6498b2fa30b6587a277ce742645
Melis Archabaev <melis99mrx@gmail.com>
2022-05-05 16:15:57 +0600

    Add Migrations

---
+++ Identity/Migrations/20220505100416_AddCity.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Identity.Migrations
+{
+    public partial class AddCity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                table: "AspNetUsers",
+                maxLength: 10,
+                defaultValue: "Bishkek",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "AspNetUsers");
+        }
+    }
+}
